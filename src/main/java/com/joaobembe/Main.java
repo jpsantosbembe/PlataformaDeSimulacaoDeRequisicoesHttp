fbdad7012b349276f1e75daa8a2f490049f9914c
João Bembe <joaobembe@icloud.com>
2024-10-16 14:58:33 -0300

    Adicionado dependencias ao projeto: Okhttp3, Dotenv

--- src/main/java/com/joaobembe/Main.java
+++ src/main/java/com/joaobembe/Main.java
@@ -1,9 +1,13 @@
 package com.joaobembe;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Dotenv dotenv = Dotenv.load();
+        final String regex = dotenv.get("REGEX");
+        System.out.println(regex);
     }
 }
